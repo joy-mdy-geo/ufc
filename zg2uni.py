5dff148ba9b20da278e0bb79856a5d782b34ed06
PyaePanSan <kidsnow102@gmail.com>
2018-07-03 20:05:14 +0800

    added places of yaychar and hatoe

--- zg2uni.py
+++ zg2uni.py
@@ -21,7 +21,7 @@ def convert(input):
     output = re.sub(u'\u108f', u'\u1014', output) # na_nge
 
     # place
-    output = re.sub(u'((?:\u1031)?)((?:\u103c)?)([\u1000-\u1021])((?:\u103d)?)((?:\u1037)?)', '\\3\\2\\4\\1\\5', output)
+    output = re.sub(u'((?:\u1031)?)((?:\u103c)?)([\u1000-\u1021])((?:\u103d)?)((?:\u103e)?)((?:\u1037)?)((?:\u102c)?)', '\\3\\2\\4\\5\\1\\6\\7', output)
 
     # pr_sint
     output = re.sub(u'\u1060', u'\u1039\u1000', output) # ka_gyi
