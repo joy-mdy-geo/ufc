d6f3d9b276e0bb159a10586dc5c3790127b6d3b0
PyaePanSan <kidsnow102@gmail.com>
2018-07-03 12:20:27 +0800

    added nga_tat

--- zg2uni.py
+++ zg2uni.py
@@ -8,5 +8,6 @@ def convert(input):
 
     output = output.replace(u'\u106a', u'\u1009')
     output = re.sub(u'[\u103b\u107e\u107f\u1080\u1081\u1082\u1083\u1084]', u'\u103c', output) # ya_yit
+    output = re.sub(u'\u1039', u'\u103A', output) #nga_tat
 
     return output
