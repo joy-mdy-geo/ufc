813132a97dcfeae525279a8b7a69ca86d1c53b69
PyaePanSan <kidsnow102@gmail.com>
2018-07-03 12:17:46 +0800

    added ya_yit

--- zg2uni.py
+++ zg2uni.py
@@ -3,8 +3,10 @@ import re
 
 
 def convert(input):
-	output = input
+    
+    output = input
 
-	output = output.replace(u'\u106A', u'\1009')
+    output = output.replace(u'\u106a', u'\u1009')
+    output = re.sub(u'[\u103b\u107e\u107f\u1080\u1081\u1082\u1083\u1084]', u'\u103c', output) # ya_yit
 
-	return output
+    return output
