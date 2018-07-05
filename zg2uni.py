0ccc395131caa86bd10049c530dff1660ccb83aa
PyaePanSan <kidsnow102@gmail.com>
2018-07-05 16:17:15 +0800

    reorder zg2uni.py

--- zg2uni.py
+++ zg2uni.py
@@ -6,15 +6,14 @@ def convert(input):
     
     output = input
     
-    output = output.replace(u'\u106a', u'\u1009')
-    output = re.sub(u'\u1025(?=[\u1039\u102c])', u'\u1009', output)
-    output = re.sub(u'\u106b', u'\u100a', output)
-    output = re.sub(u'\u1090', u'\u101b', output)
+    # replace
+    output = output.replace(u'\u106a', u'\u1009') # nya_lay
+    output = re.sub(u'\u1025(?=[\u1039\u102c])', u'\u1009', output) # nyalay_yaychar
+    output = re.sub(u'\u106b', u'\u100a', output) # nya
+    output = re.sub(u'\u1090', u'\u101b', output) # ya_guat
     output = re.sub(u'\u1033', u'\u102f', output) # ta_chuang_ngin
     output = re.sub(u'\u1034', u'\u1030', output) # na_chuang_ngin
     output = re.sub(u'[\u103d\u1087]', u'\u103e', output) # ha_toe
-    output = re.sub(u'\u1088', u'\u103e\u102f', output) #ht_tcn
-    output = re.sub(u'\u1089', u'\u103e\u1030', output) #ht_ncn
     output = re.sub(u'\u103c', u'\u103d', output) # wa_swe
     output = re.sub(u'\u1086', u'\u103f', output) # ta_gyi
     output = re.sub(u'[\u103b\u107e\u107f\u1080\u1081\u1082\u1083\u1084]', u'\u103c', output) # ya_yit
@@ -23,15 +22,20 @@ def convert(input):
     output = re.sub(u'\u104e', u'\u104e\u1044\u103a\u1038', output) # la_guang
     output = re.sub(u'[\u1037\u1094\u1095]', u'\u1037', output) # aut_myit
     output = re.sub(u'\u108f', u'\u1014', output) # na_nge
-    output = re.sub(u'\u105a', u'\u102b\u103a', output) # yaycha_shayhtoe
-    output = re.sub(u'\u108a', u'\u103d\u103e', output) # waswe_hatoe
-    # ngr_sint
+    ## nga_sint
     output = re.sub(u'([\u1000-\u1021])\u1064', u'\u1064\\1', output)
     output = re.sub(u'([\u1000-\u1021])\u108b', u'\u1064\\1\u102d', output)
     output = re.sub(u'([\u1000-\u1021])\u108c', u'\u1064\\1\u102e', output)
     output = re.sub(u'([\u1000-\u1021])\u108d', u'\u1064\\1\u1036', output)
     output = re.sub(u'\u1064', u'\u1004\u103a\u1039', output)
     output = re.sub(u'\u108e', u'\u102d\u1036', output)
+
+    # decompose
+    output = re.sub(u'\u1088', u'\u103e\u102f', output) # ha_toe and ta_chuang_ngin
+    output = re.sub(u'\u1089', u'\u103e\u1030', output) # ha_toe and na_chuang_ngin
+    output = re.sub(u'\u105a', u'\u102b\u103a', output) # yaycha_shayhtoe
+    output = re.sub(u'\u108a', u'\u102b\u103e', output) # waswe_hatoe
+
 
     # place
     ## 1=tawaetoe 2=yayit 3=letter 4=yapint 5=waswe 6=hatoe 7=aumyit 8=yaychar
