d1b715061fcd3a2f5d410b48f5cd7d1ff798ee21
PyaePanSan <kidsnow102@gmail.com>
2018-07-06 23:19:09 +0800

    change shape of aut_myit

--- for_uni2zg/uni2zg.py
+++ for_uni2zg/uni2zg.py
@@ -65,14 +65,17 @@ def precompose(input):
 def logical2visual(input):
     output = input
 
-    # 1=letters 2=yayit 3=yapint 4=waswe 5=hatoe 6=tawaetoe 7=aumyit 8=yaychar
-    output = re.sub(u'([\u1000-\u1021])((?:\u103b)?)((?:\u103a)?)((?:\u103c)?)((?:\u103d)?)((?:\u1031)?)((?:\u1037)?)((:\u102c)?)', '\\6\\2\\1\\3\\4\\5\\7\\8', output)
+    # 1=letters 2=yayit 3=yapint 4=waswe 5=hatoe 6=tawaetoe 7=nga_tat 8=aumyit 9=yaychar
+    output = re.sub(u'([\u1000-\u1021])((?:\u103b)?)((?:\u103a)?)((?:\u103c)?)((?:\u103d)?)((?:\u1031)?)((?:\u1039)?)((?:\u1037)?)((:\u102c)?)', '\\6\\2\\1\\3\\4\\5\\7\\8\\9', output)
 
     return output
 
 
 def shape(input):
     output = input
+
+    # aut_myit
+    output = re.sub(u'((?:[\u102f\u1030\u1033\u1034])?)\u1037', u'\\1\u1094', output)
 
     return output
 
