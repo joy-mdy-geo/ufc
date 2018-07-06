70277acc7287bd938b65e61b9898edcc38094b24
PyaePanSan <kidsnow102@gmail.com>
2018-07-06 14:57:40 +0800

    initialize for uni2zg convertor

---
+++ for_uni2zg/uni2zg.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+def replace(input):
+
+    output = input
+
+    return output
+
+
+def precompose(input):
+
+    output = input
+
+    return output
+
+
+def logical2visual(input):
+
+    output = input
+
+    return output
+
+
+def shape(input):
+
+    output = input
+
+    return output
+
+
+def convert(input):
+
+    output = input
+    output = replace(output)
+    output = precompose(output)
+    output = logical2visual(output)
+    output = shape(input)
+
+    return output
