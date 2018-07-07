fd961bc9c1a9b4f0192f24262aa58280e73ec954
PyaePanSan <kidsnow102@gmail.com>
2018-07-07 21:00:05 +0800

    tested with myanmar_pangram

--- for_uni2zg/test.py
+++ for_uni2zg/test.py
@@ -420,12 +420,12 @@ class TESTZG2UNI(unittest.TestCase):
         result = uni2zg.convert(unicode)
         self.assertEqual(zawgyi, result, "Failed to Convert Article Thirty")
 
-        def test_myanmar_pangram(self):
-            zawgyi = u'သီဟိုဠ္မွ ဉာဏ္ႀကီးရွင္သည္ အာယုဝၯနေဆးၫႊန္းစာကို ဇလြန္ေဈးေဘးဗာဒံပင္ထက္ အဓိ႒ာန္လ်က္ ဂဃနဏဖတ္ခဲ့သည္။'
-            unicode = u'သီဟိုဠ်မှ ဉာဏ်ကြီးရှင်သည် အာယုဝဍ္ဎနဆေးညွှန်းစာကို ဇလွန်ဈေးဘေးဗာဒံပင်ထက် အဓိဋ္ဌာန်လျက် ဂဃနဏဖတ်ခဲ့သည်။'
-            result = uni2zg.convert(unicode)
 
-            self.assertEqual(zawgyi, result, "Failed converting Myanmar Pangram")
+    def test_myanmar_pangram(self):
+        zawgyi = u'သီဟိုဠ္မွ ဉာဏ္ႀကီးရွင္သည္ အာယုဝၯနေဆးၫႊန္းစာကို ဇလြန္ေဈးေဘးဗာဒံပင္ထက္ အဓိ႒ာန္လ်က္ ဂဃနဏဖတ္ခဲ့သည္။'
+        unicode = u'သီဟိုဠ်မှ ဉာဏ်ကြီးရှင်သည် အာယုဝဍ္ဎနဆေးညွှန်းစာကို ဇလွန်ဈေးဘေးဗာဒံပင်ထက် အဓိဋ္ဌာန်လျက် ဂဃနဏဖတ်ခဲ့သည်။'
+        result = uni2zg.convert(unicode)
+        self.assertEqual(zawgyi, result, "Failed converting Myanmar Pangram")
 
 
 if __name__ == "__main__":
